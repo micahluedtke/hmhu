1a6938cfff8eb7ac5c0d524228e62a4b3a3dfb73
micahluedtke <micahluedtke@gmail.com>
2021-04-26 10:38:29 -0400

    firebase fix

--- answer.py
+++ answer.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template
 # blueprint setup
 answer = Blueprint('answer', __name__)
 
+db = firestore.client()
+
 @answer.route('/answer')
 def default():
     return render_template('answer/index.html')
--- question.py
+++ question.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template
 # blueprint setup
 question = Blueprint('question', __name__)
 
+db = firestore.client()
+
 @question.route('/question')
 def default():
     render_template('question/index.html')
