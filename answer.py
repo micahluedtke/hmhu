8cfe6d3a7faa10d353b4f94c23045c01525560b8
micahluedtke <micahluedtke@gmail.com>
2021-05-01 18:33:21 -0400

    only display questions with answers

--- answer.py
+++ answer.py
@@ -20,9 +20,9 @@ def default():
         doc_text = question[1]
         answer_ref = db.collection('questions').document(doc_id).collection('answers')
         query_ref = answer_ref.where('share', '==','1').stream()
-        answers = [ doc.to_dict()['answer'] for doc in query_ref]
+        answers = [doc.to_dict()['answer'] for doc in query_ref]
         question_answers[doc_text] = answers
-        
+    print(question_answers)
     return render_template('answer/index.html', list=question_answers)
     
 
